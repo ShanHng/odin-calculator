13bf0dc6f9c0cc3f4f20f485f6b92f7bf226e33b
ShanHng <hngshanhern@gmail.com>
2023-06-20 17:49:48 +0800

    Add operate() that runs math functions

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ function divide(a, b) {
     }
     return a / b;
 }
+
+
+let operand1;
+let operand2;
+let operator;
+
+function operate(op1, op2, operator) {
+    switch(operator) {
+        case '+':
+            return add(op1, op2);
+            break;
+        case '-':
+            return subtract(op1, op2);
+            break;
+        case '*':
+            return multiply(op1, op2);
+            break;
+        case '/':
+            return divide(op1, op2);
+            break;
+    }
+}
