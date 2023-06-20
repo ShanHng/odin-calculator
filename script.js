d4c3983b07b93ea941f69adbaa5e52732424e37c
ShanHng <hngshanhern@gmail.com>
2023-06-20 17:44:13 +0800

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,19 @@
+// Mathematical functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (!b) {
+        return 'ERROR';
+    }
+    return a / b;
+}
